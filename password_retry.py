664f9d58206ff4b52c4712ca048ce4cced0eb1f3
Willy Lee <mnak0209@gmail.com>
2020-02-04 22:07:30 +0800

    Third

--- password_retry.py
+++ password_retry.py
@@ -1,10 +1,13 @@
 x = 3
 password = 'a123456'
 while x > 0:
+	x = x-1
 	psw = input('請輸入密碼: ')
 	if psw == password:
 		print('登入成功')
 		break
 	else:
-		x = x-1
-		print('密碼錯誤,還有', x, '次機會')
+		if x > 0:
+			print('密碼錯誤','還有', x, '次機會')
+		else:
+			print('你沒機會了')
